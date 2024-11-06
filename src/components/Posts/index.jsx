686a72b774f948691396b36739b31f31bb902160
Valerii Pronin <vproninn@ya.ru>
2024-11-06 16:35:49 +0300

    Remove react fragment from Posts

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import {Post} from "./components/Post";
 import * as SC from './styles';
 
-export const Posts = ({posts}) => (
-    <>
-        <SC.Posts>
-            {posts.map((post) => <Post key={post.id} post={post}/>)}
-        </SC.Posts>
-    </>
-)
+export const Posts = ({posts}) =>
+    <SC.Posts>
+        {posts.map((post) => <Post key={post.id} post={post}/>)}
+    </SC.Posts>
